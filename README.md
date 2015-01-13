4aa2bf3658fec3ddc88c60f4fb7350ffcb146b8a
unknown <toke.jepsen@062-JINORA.baitstudio.local>
2015-01-13 08:40:29 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# atom-modo
+
+Run python scripts from atom to Modo.
+
+## Features:
+
+* Send an entire file or a selection to Modo
+
+## Installation:
+
+## Usage:
+
+Open up a python script and press ```ctrl-alt-r``` on the file or a selection.
+
+## Thanks to:
---
+++ lib/send_to_modo.py
@@ -0,0 +1,53 @@
+import socket
+import sys
+import textwrap
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="file", help="The file")
+parser.add_option("-a", "--host", dest="host", help="The host for the connection to Modo")
+parser.add_option("-p", "--port", dest="port", help="The port for the connection to Modo")
+
+(options, args) = parser.parse_args()
+
+def SendToModo(options):
+
+    PY_CMD_TEMPLATE = textwrap.dedent('''
+        import traceback
+        import __main__
+
+        namespace = __main__.__dict__.get('_atom_modo_plugin_SendToModo')
+        if not namespace:
+            namespace = __main__.__dict__.copy()
+            __main__.__dict__['_atom_modo_plugin_SendToModo'] = namespace
+
+        namespace['__file__'] = {2!r}
+
+        try:
+            {0}({1!r}, namespace, namespace)
+        except:
+            traceback.print_exc()
+	''')
+
+    command_tpl = PY_CMD_TEMPLATE.format('execfile', options.file, options.file)
+
+    host = options.host.replace('\'', '')
+    port = int(options.port)
+
+    ADDR = (host, port)
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+
+    client.send(command_tpl)
+    data = client.recv(1024)
+
+    print(data)
+
+    client.close()
+
+if __name__=='__main__':
+    if options.file:
+        SendToModo(options)
+    else:
+        sys.exit("No command given")
